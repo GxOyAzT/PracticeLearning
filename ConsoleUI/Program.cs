7a10069b47c249e176f218b2d809150ee41ad90f
Jakub Koza <jakub.koza13@gmail.com>
2020-12-01 19:17:46 +0100

    Assert.Throws<>()

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,15 +1,4 @@
 ﻿using System;
 using ClassLibrary;
 
-PersonModel person = new PersonModel()
-{
-    Id = Guid.NewGuid(),
-    FullName = "AAA",
-    Age = 55
-};
-
-PersonModel clonePerson = person.Clone();
-
-clonePerson.Age = 10;
-
 Console.WriteLine();
---
+++ XUnitTests/GetPeopleDataTest.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTests
+{
+    public class GetPeopleDataTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var db = new GetPeopleData();
+
+            Assert.Throws<NotImplementedException>(() => db.Get());
+        }
+    }
+}
