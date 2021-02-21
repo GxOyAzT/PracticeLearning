1e571e49eb954964f783fc4eae7045783d475868
Jakub Koza <jakub.koza13@gmail.com>
2021-02-21 12:12:05 +0100

    parallel

---
+++ ParallelPractice/CallApi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelPractice
+{
+    public static class CallApi
+    {
+        static HttpClient httpClient = new HttpClient();
+
+        public async static Task Call(string x)
+        {
+            var result = await httpClient.GetAsync(@"http://localhost:5000/api1.1/employee");
+            Console.WriteLine($"Call Api {x}");
+        }
+
+        public static async Task CallApiSync()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await Call("SYNC");
+            }
+        }
+
+        public async static Task CallApiAsync()
+        {
+            Parallel.For(1, 11, async e => { await Call("ASYNC"); });
+        }
+    }
+}
--- ParallelPractice/Program.cs
+++ ParallelPractice/Program.cs
@@ -1,24 +1,13 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace ParallelPractice
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            DoParallelTask.DoTenTasks();
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-
-            var watch2 = System.Diagnostics.Stopwatch.StartNew();
-            DoParallelTask.DoTenTasksParallel();
-            watch2.Stop();
-            var elapsedMs2 = watch2.ElapsedMilliseconds;
-
-            Console.WriteLine(elapsedMs);
-            Console.WriteLine(elapsedMs2);
-            Console.ReadKey();
+            
         }
     }
 }
