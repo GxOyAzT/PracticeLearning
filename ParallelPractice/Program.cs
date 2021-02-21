e6f42ad3ca04e8c1c204a241e1849493c210b337
Jakub Koza <jakub.koza13@gmail.com>
2021-02-21 10:57:49 +0100

    parallel practice

---
+++ ParallelPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ParallelPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            DoParallelTask.DoTenTasks();
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            var watch2 = System.Diagnostics.Stopwatch.StartNew();
+            DoParallelTask.DoTenTasksParallel();
+            watch2.Stop();
+            var elapsedMs2 = watch2.ElapsedMilliseconds;
+
+            Console.WriteLine(elapsedMs);
+            Console.WriteLine(elapsedMs2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ParallelPractice/RandomFileGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ParallelPractice
+{
+    public static class RandomFileGenerator
+    {
+        static string RandomString()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnoprstuwyz0123456789";
+            return new string(Enumerable.Repeat(chars, (new Random()).Next(5,1000))
+              .Select(s => s[(new Random()).Next(s.Length)]).ToArray());
+        }
+
+        static string RandomRow()
+        {
+            return $"{RandomString()},{(new Random()).Next(int.MinValue + 1, int.MaxValue - 1)},{DateTime.Now.Date.AddDays((new Random()).Next(-20000, 20000))}";
+        }
+
+        static void InsertRandomRowIntoTXT()
+        {
+            using StreamWriter file = new StreamWriter(@"C:\Data\Projects\PracticeLearning\ParallelPractice\randomData.txt", true);
+            file.WriteLine(RandomRow());
+        }
+
+        public static void InsertRowsIntoTXT(int rowsCount)
+        {
+            for (int i = 0; i < rowsCount; i++)
+            {
+                InsertRandomRowIntoTXT();
+            }
+        }
+    }
+}
