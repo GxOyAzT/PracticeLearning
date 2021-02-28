b721d6f1fd98e8d18719ec9b6f075e7da26a7512
Jakub Koza <jakub.koza13@gmail.com>
2021-02-28 15:46:32 +0100

    save file on server

---
+++ WebAppPractice/Controllers/ImagesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WebAppPractice.Controllers
+{
+    public class ImagesController : Controller
+    {
+        public ImagesController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            return View(new ImageDTO());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert(ImageDTO model)
+        {
+            if (model.File.Length > 0)
+            {
+                using (var stream = System.IO.File.Create($@"C:\Data\Projects\PracticeLearning\WebAppPractice\wwwroot\userImages\{Guid.NewGuid()}.jpg"))
+                {
+                    await model.File.CopyToAsync(stream);
+                }
+            }
+
+            return View();
+        }
+    }
+
+    public class ImageDTO
+    {
+        public IFormFile File { get; set; }
+    }
+}
