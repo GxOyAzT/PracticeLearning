c50fe9635b3e9fe2ac0897f0c308544eecd808b9
Jakub Koza <jakub.koza13@gmail.com>
2021-03-01 19:01:07 +0100

    trim ftpfile

---
+++ Algorithms.ClassLibrary/FTPLink.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.ClassLibrary
+{
+    public class FTPLink
+    {
+        public string CutString(string url)
+        {
+            return url.Substring(0, ftpDir.LastIndexOf('\\'))
+        }
+    }
+}
