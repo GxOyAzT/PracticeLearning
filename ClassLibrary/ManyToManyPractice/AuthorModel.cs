8feabbeff5af7e505c8d3e857bd493d9402b00c2
Jakub Koza <jakub.koza13@gmail.com>
2021-03-01 19:02:25 +0100

    many-to-many ef core

---
+++ ClassLibrary/ManyToManyPractice/AuthorModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary.ManyToManyPractice
+{
+    public class AuthorModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<TrackModel> TrackModels { get; set; }
+    }
+}
---
+++ ClassLibrary/ManyToManyPractice/TrackModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary.ManyToManyPractice
+{
+    public class TrackModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public ICollection<AuthorModel> AuthorModels { get; set; }
+    }
+}
