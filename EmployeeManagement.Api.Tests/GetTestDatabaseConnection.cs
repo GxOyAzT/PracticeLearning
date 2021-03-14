9311b47b61669a52228021825cdc28ccbdb8416b
Jakub Koza <jakub.koza13@gmail.com>
2021-03-14 10:12:21 +0100

    initial branch commit

--- EmployeeManagement.Api.Tests/GetTestDatabaseConnection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EmployeeManagement.Api.Tests
-{
-    public static class GetTestDatabaseConnection
-    {
-        public static string GetConnection() => @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EmployeeManagamentTests;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-    }
-}
