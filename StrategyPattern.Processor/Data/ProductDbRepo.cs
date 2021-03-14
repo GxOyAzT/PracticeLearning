e33c576301324323c9db00fa300a91792c318459
Jakub Koza <jakub.koza13@gmail.com>
2021-03-14 10:09:35 +0100

    add web

--- StrategyPattern.Processor/Data/ProductDbRepo.cs
+++ StrategyPattern.Processor/Data/ProductDbRepo.cs
@@ -1,7 +1,5 @@
 ﻿using StrategyPattern.Processor.Models;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace StrategyPattern.Processor.Data
 {
@@ -11,9 +9,9 @@ namespace StrategyPattern.Processor.Data
         {
             Products = new List<ProductDbModel>();
 
-            Products.Add(new ProductDbModel() { Id = 1, Name = "Product 1", Price = 58.99 });
-            Products.Add(new ProductDbModel() { Id = 2, Name = "Product 2", Price = 11.99 });
-            Products.Add(new ProductDbModel() { Id = 3, Name = "Product 3", Price = 199.99 });
+            Products.Add(new ProductDbModel() { Id = 1, Name = "Product 1", Price = 58.99, Quanitiy = 1 });
+            Products.Add(new ProductDbModel() { Id = 2, Name = "Product 2", Price = 11.99, Quanitiy = 1 });
+            Products.Add(new ProductDbModel() { Id = 3, Name = "Product 3", Price = 199.99, Quanitiy = 1 });
         }
 
         public List<ProductDbModel> Products { get; set; }
---
+++ StrategyPattern.Web/Controllers/BillProcessController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StrategyPattern.Processor.Data;
+using StrategyPattern.Processor.PriceCalculators;
+using StrategyPattern.Processor.Processors;
+
+namespace StrategyPattern.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BillProcessController : ControllerBase
+    {
+        private readonly IBillProcessor _billProcessor;
+        private readonly ProductDbRepo productDbRepo;
+
+        public BillProcessController(IBillProcessor billProcessor)
+        {
+            _billProcessor = billProcessor;
+            productDbRepo = new ProductDbRepo();
+        }
+
+        [HttpGet]
+        public IActionResult Get(int calculateType = -1)
+        {
+            var productList = productDbRepo.Products;
+
+            switch (calculateType)
+            {
+                case 1:
+                    _billProcessor.SetCalculator(new ProductPriceCalculator());
+                    break;
+                case 2:
+                    _billProcessor.SetCalculator(new ProductPriceCalculatorSaturdayDiscount());
+                    break;
+                case 3:
+                    _billProcessor.SetCalculator(new ProductPriceCalculatorLoyalityCard());
+                    break;
+                default:
+                    return BadRequest();
+            }
+
+            _billProcessor.GetProducts(productList);
+
+            return Ok(_billProcessor.CalculateTotal());
+        }
+    }
+}
