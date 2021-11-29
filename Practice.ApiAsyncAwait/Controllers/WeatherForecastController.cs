05c0c63a2d7259946e377140c927e4ccd4c44e1e
Jakub Koza <jakub.koza13@gmail.com>
2021-11-29 17:02:00 +0100

    some changes

---
+++ Practice.ApiAsyncAwait/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Practice.ApiAsyncAwait.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("a")]
+        public IActionResult GetA()
+        {
+            x();
+            return Ok("aa");
+        }
+
+        [HttpGet]
+        [Route("b")]
+        public IActionResult GetB()
+        {
+            x();
+            return Ok("bb");
+        }
+
+        private void x()
+        {
+            Thread.Sleep(5000);
+        } 
+    }
+}
