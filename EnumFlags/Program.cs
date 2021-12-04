60a846763b9517ee185b7cd79add80c4851bcaa5
Jakub Koza <jakub.koza13@gmail.com>
2021-12-04 16:53:37 +0100

    enum flags

---
+++ EnumFlags/Program.cs
@@ -0,0 +1,17 @@
+﻿var animal = Animal.Cat | Animal.Elephant;
+
+Console.WriteLine(animal);
+
+Console.WriteLine(animal.HasFlag(Animal.Cat));
+Console.WriteLine(animal.HasFlag(Animal.Dog));
+Console.WriteLine(animal.HasFlag(Animal.Elephant));
+Console.WriteLine(animal.HasFlag(Animal.Bee));
+
+enum Animal
+{
+    None = 0,
+    Cat = 1,
+    Dog = 2,
+    Elephant = 4,
+    Bee = 8,
+}
